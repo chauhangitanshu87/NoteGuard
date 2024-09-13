7b2dada60eb7fd5ec79d1cf00c5e39eec12106d1
girishrana <ranagirish246@gmail.com>
2024-09-13 22:07:08 +0530

    6 commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useContext, useEffect } from 'react'
+import noteContext from '../context/noteContext'
+
+const About = () => {
+    
+    return (
+        <div>
+            This is About page
+        </div>
+    )
+}
+
+export default About
